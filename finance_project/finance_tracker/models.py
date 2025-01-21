e6200513866bb9d4ad4f7c72744f2cfa01959b8e
Graham <gblackledge88@gmail.com>
2025-01-22 00:32:20 +1300

    add balance and user

--- finance_project/finance_tracker/models.py
+++ finance_project/finance_tracker/models.py
@@ -9,6 +9,6 @@ class Account(models.Model):
     ]
     name = models.CharField(max_length=100)
     account_type = models.CharField(max_length=3, choices=ACCOUNT_TYPES)
-    # balance
-    # user
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
