f3e988b91bdfa22646c606d755476b18d2b35978
Graham <gblackledge88@gmail.com>
2025-01-23 00:13:36 +1300

    add Transaction model

--- finance_project/finance_tracker/models.py
+++ finance_project/finance_tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Account(models.Model):
@@ -11,4 +12,22 @@ class Account(models.Model):
     account_type = models.CharField(max_length=3, choices=ACCOUNT_TYPES)
     balance = models.DecimalField(max_digits=12, decimal_places=2)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    type = models.CharField(max_length=100)
     
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense, Expense'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    transaction_type = models.CharField(max_length=20, choices=TRANSACTION_TYPES)
+    time_of_transaction = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.transaction_type} of {self.amount} on {self.time_of_transaction}"
+
