9b334b92a340ea25d79545e65e85fab7eee016d2
Graham <gblackledge88@gmail.com>
2025-01-25 00:19:14 +1300

    created superuser

--- finance_project/finance_tracker/admin.py
+++ finance_project/finance_tracker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Account, BudgetGoal, Transaction, Asset, Category
+from .models import Account, BudgetGoal, Transaction, Asset, Category
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(BudgetGoal)
