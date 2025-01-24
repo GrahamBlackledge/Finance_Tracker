542eb58b46c1c7d9d3fc63c8b27fe8516f5433b5
Graham <gblackledge88@gmail.com>
2025-01-25 00:09:51 +1300

    registerd models in admin.py

--- finance_project/finance_tracker/admin.py
+++ finance_project/finance_tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from models import Account, BudgetGoal, Transaction, Asset, Category
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(BudgetGoal)
+admin.site.register(Transaction)
+admin.site.register(Asset)
+admin.site.register(Category)
