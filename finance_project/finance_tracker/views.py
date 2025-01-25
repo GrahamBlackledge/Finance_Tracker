ad34d307a1f795ea744f2c486f3b9841cc60f22d
Graham <gblackledge88@gmail.com>
2025-01-25 15:59:46 +1300

    set up views and add TransactionListView

--- finance_project/finance_tracker/views.py
+++ finance_project/finance_tracker/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Transaction
 # Create your views here.
+
+class TransactionListView(LoginRequiredMixin, ListView):
+    model = Transaction
+    template_name = 'finance_tracker/transaction_list.html'
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
