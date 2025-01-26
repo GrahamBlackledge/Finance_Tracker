93ef2e712bd02820d3297b2d567c85236f96c1ea
Graham <gblackledge88@gmail.com>
2025-01-26 15:39:05 +1300

    add creat transaction view

--- finance_project/finance_tracker/views.py
+++ finance_project/finance_tracker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Transaction
+from django.urls import reverse_lazy
 # Create your views here.
 
 class TransactionListView(LoginRequiredMixin, ListView):
@@ -10,3 +11,9 @@ class TransactionListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Transaction.objects.filter(user=self.request.user)
+
+class TransactionCreateView(LoginRequiredMixin, CreateView):
+    model = Transaction
+    fields = 'category', 'transaction_type', 'amount', 'time_of_transaction', 'description', 'is_recurring'     
+    template_name = 'finance_tracker/transaction_form.html'
+    success_url = reverse_lazy('transaction_list')
